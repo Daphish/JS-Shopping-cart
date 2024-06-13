8a6f660de619386d4023cd4b9d53dffb36755a2f
Ivan <ivan.davila.2003@gmail.com>
2024-06-13 12:32:55 -0600

    Se agrega al carrito la inforamación del curso clickeado

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners(){
@@ -19,12 +20,56 @@ function agregarCurso(e){
 }
 
 // Lee el contenido del html y extraer la información del curso
-function leerDatosCurso(){
+function leerDatosCurso(curso){
     const infoCurso = {
         imagen: curso.querySelector('img').src,
         titulo: curso.querySelector('h4').textContent,
         precio: curso.querySelector('.precio span').textContent,
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
+    }
+
+    //Agregar elementos al arreglo del carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    carritoHTML();
+}
+
+// Muestra el carrito de compras con HTML
+function carritoHTML(){
+    // Limpiar el HTML
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach( curso => {
+        const { imagen, titulo, precio, cantidad, id} = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${imagen}" width="100">
+            </td>
+            <td>
+                ${titulo}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                ${cantidad}
+            </td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}"> X </a>
+            </td>
+        `;
+
+        // Agregar el HTML al carrito en el body
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+//Elimina los cursos del tbody
+function limpiarHTML(){
+    //contenedorCarrito.innerHTML = '';
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
     }
 }
